84683250bda69f85c30721a3765e1187645d0533
quanggvph17758 <80687998+quanggvph17758@users.noreply.github.com>
2022-08-16 15:03:56 +0700

    update

---
+++ src/app/User/edit/edit.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserModel } from 'src/app/Model/UserModel';
+import { UserServiceService } from 'src/app/Service/user-service.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit {
+
+  constructor(private router:Router, private service:UserServiceService) { }
+
+  exform!: FormGroup;
+
+  ngOnInit() {
+    this.Edit();
+    this.exform = new FormGroup({
+      'fullname': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      'birthday': new FormControl(null, Validators.required),
+      'gender': new FormControl(null, Validators.required),
+      'address': new FormControl(null, Validators.required),
+    });
+  }
+
+  user:UserModel = new UserModel();
+
+  Edit() {
+    let id = localStorage.getItem("id");
+    this.service.getUserId(Number(id))
+    .subscribe(data => {
+      this.user=data;
+    });
+  }
+
+  Update() {
+    this.service.updateUser(this.user)
+    .subscribe(data => {
+      this.user = data;
+      alert("Update Thành Công!");
+      this.router.navigate(["list"]);
+    });
+  }
+}
